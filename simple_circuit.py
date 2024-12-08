eec6f8c84094871d7f0dcea8c35c02c2c07b27f6
Bill Skarlatos <skarlatosbill@gmail.com>
2024-12-08 14:45:08 +0200

    Simple Quantum Circuit

---
+++ simple_circuit.py
@@ -0,0 +1,29 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import RXGate, RYGate, RZGate
+import numpy as np
+
+# Parameters
+t = 1.0  # Evolution time
+n_trotter = 5  # Number of Trotter steps
+
+# Define the circuit
+qc = QuantumCircuit(1)
+
+# Define time evolution for H0, V1, and V2
+def unperturbed_evolution(circuit, t):
+    circuit.append(RXGate(2 * t), [0])  # Implement e^{-iH_0t}
+
+def perturbation_1(circuit, t):
+    circuit.append(RYGate(2 * t), [0])  # Implement e^{-iV_1t}
+
+def perturbation_2(circuit, t):
+    circuit.append(RZGate(2 * t), [0])  # Implement e^{-iV_2t}
+
+# Trotterization
+for _ in range(n_trotter):
+    unperturbed_evolution(qc, t / n_trotter)
+    perturbation_1(qc, t / n_trotter)
+    perturbation_2(qc, t / n_trotter)
+
+# Draw the circuit
+print(qc.draw())
